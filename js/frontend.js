da8d31ff10aba8a92777d676cc4b489bf821b7cf
Steven <stevenhughes08@gmail.com>
2020-09-27 20:43:10 -0400

    added imagaes and many changes to the code

--- js/frontend.js
+++ js/frontend.js
@@ -1,18 +1,41 @@
+// Nav Menu
+
+
+
+// Bars menu
+fuction showHideMenu() {
+    const burger = document.getElementById("burgerBtn");
+    if (burger.style.display === "none") {
+        burger.style.display = "block";
+    } else {
+        burger.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
 // Image slideshow
 
-var navItems = ['Home', 'Characters',  'Booking', 'Tip Jar']
+var navItems = ['Home', 'Characters', 'Booking', 'Tip Jar']
 
 const rotatingImgs = document.getElementsByClassName('slide-img');
 let i = 0;
 
-setInterval(function(){
-if(i == 0){
-    rotatingImgs[i].style.display = 'block';
-} else {
-    rotatingImgs[i - 1].style.display = 'none';
-    rotatingImgs[i].style.display = 'block';
-} 
+setInterval(function() {
+    if (i == 0) {
+        rotatingImgs[i].style.display = 'block';
+    } else {
+        rotatingImgs[i - 1].style.display = 'none';
+        rotatingImgs[i].style.display = 'block';
+    }
 
-i++;
+    i++;
 
 }, 2000);
