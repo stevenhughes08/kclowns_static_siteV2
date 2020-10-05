4680aada3ffdce385e6a37075ec3682b232019ca
Steven <stevenhughes08@gmail.com>
2020-10-05 18:18:42 -0400

    added vue files andependencies

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="app">
+    <Sidebar />
+</div>
+</template>
+
+<script>
+import Sidebar from './components/sidebar.vue'
+
+export default {
+    components: {
+        Sidebar
+    }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Anton');
+@import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+
+body {
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    font-family: 'Anton', Arial, Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    color: #322F42;
+    width: 100%;
+    min-height: 100vh;
+}
+</style>
